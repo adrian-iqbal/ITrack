9343aee07dd0707c3a4e310dc0e1383f3a8882dd
adrian-iqbal <iqbaladriam@gmail.com>
2025-11-05 20:38:14 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-center">Hello world</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
